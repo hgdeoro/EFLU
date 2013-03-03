7e2f00f712eeddc29df4e7273d7fb76d1335a809
Horacio G. de Oro <hgdeoro@gmail.com>
2013-03-03 13:31:07 -0300

    Add handling of RabbitMQ events

--- eyefilinuxui/gui/newui.py
+++ eyefilinuxui/gui/newui.py
@@ -4,10 +4,16 @@ Created on Mar 2, 2013
 @author: Horacio G. de Oro
 '''
 
+import json
+import logging
+import pika
 import sys
 
 import PySide
+
 from PySide import QtCore, QtGui
+from pika.exceptions import AMQPConnectionError
+
 
 # http://stackoverflow.com/questions/13302908/better-way-of-going-from-pil-to-pyside-qimage
 sys.modules['PyQt4'] = PySide # HACK for ImageQt
@@ -15,7 +21,47 @@ sys.modules['PyQt4'] = PySide # HACK for ImageQt
 import Image
 import ImageQt
 
+from eyefilinuxui.util import EVENT_QUEUE_NAME
 from eyefilinuxui.gui.ui.mainwindow_ui import Ui_MainWindow
+
+
+class RabbitMQEventReaderThread(QtCore.QThread):
+
+    def __init__(self, parent=None):
+        QtCore.QThread.__init__(self, parent)
+
+    def callback(self, ch, method, properties, msg):
+        logging.info("MSG recibido: %s", msg)
+        msg = json.loads(msg)
+        if msg['origin'] == 'eyefiserver' and msg['type'] == 'upload' and msg['extra']:
+            if 'filename' in msg['extra']:
+                self.emit(
+                    QtCore.SIGNAL("display_image(QString)"),
+                    msg['extra']['filename'],
+                )
+
+        #    send_event('eyefiserver', 'upload', {
+        #        'filename': imagePath,
+        #    })
+        #    msg = {
+        #        'origin': origin,
+        #        'type': event_type,
+        #        'extra': extra,
+        #    }
+
+    def run(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+        channel.exchange_declare(exchange=EVENT_QUEUE_NAME, type='fanout')
+        result = channel.queue_declare(exclusive=True)
+        _queue_name = result.method.queue
+        channel.queue_bind(exchange=EVENT_QUEUE_NAME, queue=_queue_name)
+
+        channel.basic_consume(self.callback, queue=_queue_name, no_ack=True)
+        try:
+            channel.start_consuming()
+        except AMQPConnectionError:
+            raise
 
 
 #
@@ -35,6 +81,13 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         self.imageQt = None
         self.pixMap = None
 
+        # Create thread and connect
+        self.rabbitmq_reader_thread = RabbitMQEventReaderThread()
+        self.connect(self.rabbitmq_reader_thread,
+            QtCore.SIGNAL("display_image(QString)"),
+            self.display_image)
+        self.rabbitmq_reader_thread.start()
+
     def _do_resize(self):
         if self.image:
             w, h = self.image.size
@@ -53,9 +106,14 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         self._do_resize()
 
 
-if __name__ == '__main__':
+def start_gui():
     app = QtGui.QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    window.display_image(sys.argv[1])
+    # window.display_image(sys.argv[1])
+    # app.exec_()
+    return app, window
+
+if __name__ == '__main__':
+    app, window = start_gui()
     sys.exit(app.exec_())
