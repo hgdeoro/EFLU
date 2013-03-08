2ee807d9e8766e10ce27b4b43e086f25d86c82da
Horacio G. de Oro <hgdeoro@gmail.com>
2013-03-08 16:57:34 -0300

    Add copyright and license to files

--- eyefilinuxui/tests/tests_hostapd.py
+++ eyefilinuxui/tests/tests_hostapd.py
@@ -1,8 +1,23 @@
-'''
-Created on Mar 2, 2013
+# coding=utf-8
 
-@author: Horacio G. de Oro
-'''
+#----------------------------------------------------------------------
+# Copyright (c) 2013 Horacio G. de Oro <hgdeoro@gmail.com>
+#----------------------------------------------------------------------
+#    This file is part of EFLU.
+#
+#    EFLU is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    EFLU is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with EFLU.  If not, see <http://www.gnu.org/licenses/>.
+#----------------------------------------------------------------------
 
 import logging
 import time
